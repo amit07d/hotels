17bd2385651592d18c6096b28acfc4802b8d1053
amit07d <dnathamit2003@gmail.com>
2024-10-04 17:56:06 +0530

    error correction

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('./../models/person'); // Import the Person model
+const Person = require('./../models/Person'); // Import the Person model
 
 // POST method to create a person
 router.post('/', async (req, res) => {
